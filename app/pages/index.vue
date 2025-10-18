45ab9f92eda43abc9a63bcaa4f7f8df1b3875f54
Thijs <thijsvanwaveren@outlook.com>
2025-10-18 21:21:06 +0200

    Fixed API filename fetcher to work in front-end. Added simple body styling and text styling

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,32 +1,51 @@
 <script setup>
   const { data } = await useFetch('/api/fetch-img')
+
+  // Body styling
+  useHead({
+    bodyAttrs: {
+      class: 'bg-[#FFF8E7]'
+    }
+  })
 </script>
 
 
 <template>
   <section class="max-w-5xl mx-auto px-4 py-12">
     <div class="text-center mt-30 mb-50">
-      <h2 class="text-3xl md:text-4xl font-bold mb-4 text-gray-800">
+      <nav>
+        <ul>
+          <li><NuxtLink to="/projects">Projects</NuxtLink></li>
+        </ul>
+      </nav>
+
+
+      <h2 class="text-3xl md:text-[10rem] font-bold mb-4 text-gray-800">
         Thijs.dev
       </h2>
-      <p class="text-gray-600 max-w-2xl mx-auto">
-        A simple, responsive layout built with Nuxt 3 and TailwindCSS. 
-        Perfect as a base for landing pages or apps.
-      </p>
+
+      <div class="flex space-x-2 justify-center">
+        <a href="https://github.com/Thijs861">
+          <img class="w-10 h-auto" src="./icons/git-ico/github-mark.png" alt="Github icon">
+        </a>
+        <a href="https://www.figma.com/@Thijsvw">
+          <img class="w-10 h-auto" src="./icons/fig-ico/icons8-figma-100.png" alt="Figma icon">
+        </a>
+      </div>
     </div>
 
     <!-- Grid Section -->
-    <div class="grid gap-6 grid-cols-1 md:grid-cols-3">
-      <div 
-        v-for="file in data?.files"
-        :key="file"
-        class="p-6 bg-white rounded-2xl shadow hover:shadow-md transition">
-        <img :src="`../assets/img/${file}`" :alt="file" class="w-full h-auto rounded-lg">
+    <div class="text-center space-y-5">
+      <p class="font-bold text-2xl">Check out some of my projects</p>
+      <div class="grid gap-6 grid-cols-1 md:grid-cols-3">
+        <div
+          v-for="file in data?.files"
+          :key="file"
+          class="p-6 bg-white rounded-2xl shadow hover:shadow-md transition">
+          <img :src="`../assets/img/${file}`" :alt="file" class="w-full h-auto rounded-lg">
+        </div>
       </div>
-      
     </div>
-
-    <img src="../assets/img/WeDoTech.png" alt="test">
 
     <!-- What i use to code -->
     <div>
