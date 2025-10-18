34cf2896534517f63dee658671c78aec2a9f1fdc
Thijs <thijsvanwaveren@outlook.com>
2025-10-18 14:11:07 +0200

    Deleted files

--- server/api/fetch-img.get.js
+++
@@ -1,9 +0,0 @@
-import { readdirSync } from 'fs';
-import { join } from 'path';
-
-
-export default defineEventHandler(() => {
-    const folderPath = join(process.cwd(), 'public/assets/img')
-    const files = readdirSync(folderPath)
-    return { files }
-})
