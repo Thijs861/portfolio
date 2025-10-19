972f302fcfa4ff1c5962add76ce7cc7af219fa51
Thijs <thijsvanwaveren@outlook.com>
2025-10-19 11:29:44 +0200

    Fixed css crashing website issue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+// import { defineNuxtConfig, resolve } from 'nuxt/config'
+import path from "path"
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  css: ['@/assets/css/main.css'],
+  css: [path.resolve(__dirname, "assets/css/main.css")],
   vite: {
     plugins: [
       tailwindcss(),
