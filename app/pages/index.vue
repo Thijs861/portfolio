2583072e05b5ce8e70707d69b7bed90857826433
Thijs <thijsvanwaveren@outlook.com>
2025-10-24 20:43:52 +0200

    Moved skills & projects to different route. Moved API hook to app file

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,58 +1,19 @@
-<script setup>
-  const { data } = await useFetch('/api/fetch-img')
-
-  // Body styling
-  useHead({
-    bodyAttrs: {
-      class: 'bg-[#FFF8E7]'
-    }
-  })
-</script>
-
-
 <template>
-  <section class="max-w-5xl mx-auto px-4 py-12">
-    <div class="text-center mt-30 mb-50">
-      <nav>
-        <ul>
-          <li><NuxtLink to="/projects">Projects</NuxtLink></li>
-        </ul>
-      </nav>
-
-
-      <h2 class="text-3xl md:text-[10rem] font-bold mb-4 text-gray-800">
+  <section class="hero flex items-center justify-center mt-[200px]">
+    <div class="text-center">
+      <h2 class="text-3xl md:text-[10rem] font-bold mb-4 text-gray-200 bungee">
         Thijs.dev
       </h2>
 
-      <div class="flex space-x-2 justify-center">
+      <p class="text-2xl font-bold text-gray-300 roboto-mono">Build whatever comes to mind ✨</p>
+      <div class="flex space-x-2 justify-center mt-10">
         <a href="https://github.com/Thijs861">
-          <img class="w-10 h-auto" src="./icons/git-ico/github-mark.png" alt="Github icon">
+          <img class="w-10 h-auto" src="/icons/git-ico/github-mark-white.png" target="_blank" alt="Github icon">
         </a>
-        <a href="https://www.figma.com/@Thijsvw">
-          <img class="w-10 h-auto" src="./icons/fig-ico/icons8-figma-100.png" alt="Figma icon">
-        </a>
+        <!-- <a href="https://www.figma.com/@Thijsvw">
+          <img class="w-10 h-auto" src="/icons/fig-ico/icons8-figma-100.png" alt="Figma icon">
+        </a> -->
       </div>
     </div>
-
-    <!-- Grid Section -->
-    <div class="text-center space-y-5">
-      <p class="font-bold text-2xl">Check out some of my projects</p>
-      <div class="grid gap-6 grid-cols-1 md:grid-cols-3">
-        <div
-          v-for="file in data?.files"
-          :key="file"
-          class="p-6 bg-white rounded-2xl shadow hover:shadow-md transition">
-          <img :src="`../assets/img/${file}`" :alt="file" class="w-full h-auto rounded-lg">
-        </div>
-      </div>
-    </div>
-
-    <!-- What i use to code -->
-    <div>
-
-    </div>
-
-
   </section>
-
 </template>
