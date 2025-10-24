61c1874e0c164002f30e1205b3cb55875f2ff2bd
Thijs <thijsvanwaveren@outlook.com>
2025-10-24 20:37:12 +0200

    Added Nuxt modules to project/config

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
+  modules: ['@nuxt/fonts'],
   app: {
     head: {
       title: 'Thijs.dev',
