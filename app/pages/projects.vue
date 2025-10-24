0006c04a024c29ba78cd05b95a4975192f21eaa2
Thijs <thijsvanwaveren@outlook.com>
2025-10-24 20:45:38 +0200

    Created projects route.

--- app/pages/projects.vue
+++ app/pages/projects.vue
@@ -1,13 +1,19 @@
-<template>
-    <div>
-        <header>
-            <nav>
-                <ul>
-                    <li><NuxtLink to="/">Home</NuxtLink></li>
-                </ul>
-            </nav>
-        </header>
+<script setup>
+  const { data } = await useFetch('/api/fetch-img')
+</script>
 
-        <h1 class="font-bold text-3xl">Welcome to my blog. </h1>
-    </div>
+<template>
+    <section class="mb-10">
+      <div class="space-y-5">
+        <p class="font-bold text-2xl test2 text-gray-200">Projects</p>
+        <div class="grid gap-6 grid-cols-1 md:grid-cols-3">
+          <div
+            v-for="file in data?.files"
+            :key="file"
+            class="p-6 bg-white rounded-2xl shadow hover:shadow-md transition">
+            <img :src="`../assets/img/thumbnails/${file}`" :alt="file" class="w-full h-auto rounded-lg">
+          </div>
+        </div>
+      </div>
+    </section>
 </template>
