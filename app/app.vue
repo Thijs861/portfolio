94fdb8c85f3476f5bc4768eb8fa1e546a83bdfa6
Thijs <thijsvanwaveren@outlook.com>
2025-10-25 19:21:45 +0200

    Removed footer, added extra paddin g to header tag

--- app/app.vue
+++ app/app.vue
@@ -20,10 +20,10 @@
 <template>
   <main class="max-w-5xl mx-auto px-4 py-12">
     <header class="flex justify-center items-center py-4 bg-[#1e1e1e]">
-      <nav class="flex items-center gap-8 bg-[#2a2a2a] border border-[#333] rounded-full px-10 py-3 text-gray-300 shadow-lg backdrop-blur-sm roboto-mono">
+      <nav class="flex items-center gap-8 bg-[#2a2a2a] border border-[#333] rounded-full px-10 py-3 text-gray-300 shadow-lg backdrop-blur-sm roboto">
         <a href="/" class="hover:text-white transition-colors duration-200 ">Home</a>
         <a href="/projects" class="hover:text-white transition-colors duration-200">Projects</a>
-        <a href="#" class="hover:text-white transition-colors duration-200 line-through">About</a>
+        <p class="line-through">About</p>
         <a href="/skills" class="hover:text-white transition-colors duration-200">Skills</a>
       </nav>
     </header>
@@ -32,9 +32,5 @@
         <NuxtPage />
       </NuxtLayout>
     </div>
-
-  <!-- <footer class="text-center">
-    <p>&copy; 2025</p>
-  </footer> -->
   </main>
 </template>
