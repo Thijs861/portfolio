94f1e07678393c3e0ba62936c9a2e429391e3789
Thijs <thijsvanwaveren@outlook.com>
2025-11-01 14:08:51 +0100

    changed gap to space-y

--- app/app.vue
+++ app/app.vue
@@ -20,8 +20,8 @@
 <template>
   <main class="max-w-5xl mx-auto px-4 py-12">
     <header class="flex justify-center items-center py-4 bg-[#1e1e1e]">
-      <nav class="flex items-center gap-8 bg-[#2a2a2a] border border-[#333] rounded-full px-10 py-3 text-gray-300 shadow-lg backdrop-blur-sm roboto">
-        <a href="/" class="hover:text-white transition-colors duration-200 ">Home</a>
+      <nav class="flex items-center space-x-8 bg-[#2a2a2a] border border-[#333] rounded-full px-10 py-3 text-gray-300 shadow-lg backdrop-blur-sm roboto">
+        <a href="/" class="hover:text-white transition-colors duration-200">Home</a>
         <a href="/projects" class="hover:text-white transition-colors duration-200">Projects</a>
         <p class="line-through">About</p>
         <a href="/skills" class="hover:text-white transition-colors duration-200">Skills</a>
